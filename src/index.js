636ed1abc58f052aefbec933899581b16fbbea10
root <root@DESKTOP-6DGOVDG.localdomain>
2020-07-11 18:39:06 -0400

    put index.html in the project folder. Testing if path to index.js is valid when html is published on github

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// simple front end
+// by Bill Shi
+
+let placeholder = ['CMSC216','CMSC250']
+
+//$('#courses').length
+// 0 when element is not loaded
+
+// problem: can't modify html elements until they are loaded
+// quick fix: use document.ready to wait
+$(document).ready(function () {
+	
+	for (course of placeholder) {
+		$('#courses').append(`<li>${course}</li>`)
+	}
+	
+	/*
+	// read and display infomation from class_info.json
+	$.getJSON('./output/class_info.json', function (class_info) {
+		
+		let in_person = class_info['in person'];
+		
+		// only display courses from the AASP department
+		let department = 'AASP';
+		
+		for (course in in_person[department]) {
+			
+			console.log(course);
+			
+			let course_container = $(`<li><h3>${course}</h3></li>`);
+			
+			let section_list = $('<ul></ul>');
+			for (section in in_person[department][course]) {
+				section_list.append(`<li>${section} Seating: \
+				${in_person[department][course][section]}</li>`)
+			}
+			
+			course_container.append(section_list);
+			$('#courses').append(course_container);
+		}
+		
+		
+	})
+	*/
+
+
+})
