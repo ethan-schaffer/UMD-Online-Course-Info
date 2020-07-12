0a301f9535ef9f5a49c9776ea8a005aa9568718a
Bill Shi <jhshi31@gmail.com>
2020-07-12 16:40:13 -0400

    made a basic look up function for classes, no styling yet

--- src/index.js
+++ src/index.js
@@ -1,42 +1,74 @@
 // simple front end
 // by Bill Shi
 
-let placeholder = ['CMSC216','CMSC250']
 
-//$('#courses').length
-// 0 when element is not loaded
+let in_person = class_info['in person'];
+let online = class_info['online'];
 
-// problem: can't modify html elements until they are loaded
-// quick fix: use document.ready to wait
+// list of department codes
+// should be hardcoded
+let dept_codes = [];
+for (department in in_person) {
+	dept_codes.push(department);
+}
+
+
+function createCourseContainer (course, course_obj) {
+	let course_container = 	$(`<div class='course_container'>
+								<h3>${course}</h3>
+								<ul></ul>
+							</div>`);
+	let ul = $(course_container, 'ul');
+	
+	for (section in course_obj) {
+		let section_string = section.substring(1,section.length-1)
+							.replace(/'/g,'');
+		let class_time = section_string.split(',');
+		
+		let li = 	$(`<li>
+						<p>Meeting Time: ${class_time[0]} ${class_time[1]} -
+						${class_time[2]}</p>
+						<p>Seats (Total): ${course_obj[section]}</p>
+					</li>`);
+		ul.append(li);
+	}
+	
+	return course_container;
+}
+
+// wait until html elements are ready
 $(document).ready(function () {
 	
-	/*
-	for (course of placeholder) {
-		$('#courses').append(`<li>${course}</li>`)
-	}
-	*/
-	
-
-	let in_person = class_info['in person'];
-	
-	// only display courses from the AASP department
 	let department = 'AASP';
+	let course = 'AASP100';
+	let course_obj = in_person[department][course];
 	
-	for (course in in_person[department]) {
+	$('#search-return').append(createCourseContainer(course, course_obj));
+	
 		
-		//console.log(course);
+	
+	$('#class-lookup').on('input', function () {
+		let usr_input = $(this).val();
 		
-		let course_container = $(`<li><h3>${course}</h3></li>`);
-		
-		let section_list = $('<ul></ul>');
-		for (section in in_person[department][course]) {
-			section_list.append(`<li>${section} Seating: \
-			${in_person[department][course][section]}</li>`)
+		if (usr_input.length == 0) {
+			$('#search-return').empty();
 		}
 		
-		course_container.append(section_list);
-		$('#courses').append(course_container);
-	}
-
+		if (usr_input.length == 4) {
+			
+			if (dept_codes.includes(usr_input)) {
+				$('#search-return').empty();
+				
+				let department = usr_input;
+				
+				for (course in in_person[department]) {
+					let course_obj = in_person[department][course];
+					$('#search-return').append(createCourseContainer(course, course_obj));
+				}
+				
+			}
+			
+		}
+	})
 
 })
