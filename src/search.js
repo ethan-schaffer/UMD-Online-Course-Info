7afdcd3942ec1f19c6075c0abf11a70795b53133
Bill Shi <jhshi31@gmail.com>
2020-07-14 18:35:59 -0400

    implemented filtering by instuction type

--- src/search.js
+++ src/search.js
@@ -17,6 +17,8 @@ function emptySearchFieldInfo() {
 function createCourseContainer(course_code) {
 	let course_info = catalog[course_code];
 	
+	let learning_mode_filter = $('#filter-learning-mode').val();
+	
 	// creates course container
 	let course_container = 	$(`<div class='course-container'>
 								<h3>${course_code +' '+ course_info['course-name']}</h3>
@@ -24,36 +26,71 @@ function createCourseContainer(course_code) {
 							</div>`);
 	let ul = $(course_container, 'ul');
 	
-	
 	// adds infomation about class sections to ul
 	let sections = course_info['sections'];
 	for (section_code in sections) {
 		
-		// create text about lab/discussion meething time
-		let lab_info = '';
-		if(sections[section_code]['lab-time'] != null) { 
-			lab_info = "Additional Section:<br>" + sections[section_code]['lab-time']
+		// append sections according to learning_mode_filter
+		if (learning_mode_filter == 'all' || 
+			learning_mode_filter == sections[section_code]['learning-mode']) {
+			
+			// create text about lab/discussion meething time
+			let lab_info = '';
+			if(sections[section_code]['lab-time'] != null) { 
+				lab_info = "Additional Section:<br>" + sections[section_code]['lab-time'];
+			}
+			
+			// create section data html container
+			let section = 	$(`<li>
+								<table class="section_table">
+									<tr>
+										<td>Section:<br>${section_code}</td>
+										<td>Total Seats:<br>${sections[section_code]['capacity']}</td>
+										<td>Taught by:<br>${sections[section_code]['instructor']}</td>
+										<td>Lecture Time:<br>${sections[section_code]["lecture-time"]}</td>
+										<td>${lab_info}</td>
+										<td>Learning Type:<br>${sections[section_code]['learning-mode']}</td>
+										<td>Seats Open:<br>${sections[section_code]['open-seats']}</td>
+									</tr>
+								</table>
+							</li>`);
+						
+			ul.append(section);
 		}
 		
-		// create section data html container
-		let section = 	$(`<li>
-							<table class="section_table">
-								<tr>
-									<td>Section:<br>${section_code}</td>
-									<td>Total Seats:<br>${sections[section_code]['capacity']}</td>
-									<td>Taught by:<br>${sections[section_code]['instructor']}</td>
-									<td>Lecture Time:<br>${sections[section_code]["lecture-time"]}</td>
-									<td>${lab_info}</td>
-									<td>Learning Type:<br>${sections[section_code]['learning-mode']}</td>
-									<td>Seats Open:<br>${sections[section_code]['open-seats']}</td>
-								</tr>
-							</table>
-						</li>`);
-					
-		ul.append(section);
 	}
 	
+	// TODO when ul has not li children, do not return a course-container
+	
 	return course_container;
+}
+
+//
+function executeSearch() {
+	let user_input = $('#class-lookup').val().toUpperCase();
+		
+	// clears search results
+	$('#search-return').empty();
+
+	//
+	if (user_input == ""){
+		$('#search-return').append(emptySearchFieldInfo());
+	}
+	
+	for (course in catalog) {
+		
+		//
+		if (user_input == course) {
+			$('#search-return').append(createCourseContainer(course));
+			return;
+		}
+		
+		//
+		if (user_input == catalog[course]['department']) {
+			$('#search-return').append(createCourseContainer(course));
+		}
+		
+	}
 }
 
 // wait until html elements are ready and execute code within
@@ -62,33 +99,9 @@ $(document).ready(function () {
 	$('#search-return').append(emptySearchFieldInfo());
 	
 	// create behavior for when text is entered in the input box
-	$('#class-lookup').on('input', function () {
+	$('#class-lookup').on('input', executeSearch);
 	
-		let user_input = $(this).val().toUpperCase();
-		
-		// clears search results
-		$('#search-return').empty();
-
-		//
-		if (user_input == ""){
-			$('#search-return').append(emptySearchFieldInfo());
-		}
-		
-		for (course in catalog) {
-			
-			//
-			if (user_input == course) {
-				$('#search-return').append(createCourseContainer(course));
-				return;
-			}
-			
-			//
-			if (user_input == catalog[course]['department']) {
-				$('#search-return').append(createCourseContainer(course));
-			}
-			
-		}
-		
-	})
+	//
+	$('#filter-learning-mode').on('change', executeSearch);
 
 })
