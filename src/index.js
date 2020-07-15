060de8a2d17de9289e64e3b2a4627a42959b1297
Ethan Schaffer <ethan@schaffer.com>
2020-07-14 23:25:08 -0500

    Language and Color Tweaks, Move Dept Breakdown to Own page

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ function getFooterContent() {
 
 function getChartUrl(in_person, online) {
     console.log(in_person, online);
-    let url = "https://quickchart.io/chart?bkg=white&c=%7Btype%3A%27pie%27%2Cdata%3A%7Bdatasets%3A%5B%7Bdata%3A%5B"+ in_person + "%2C" + online + "%5D%2CbackgroundColor%3A%5B%27rgb(255%2C99%2C132)%27%2C%27rgb(54%2C162%2C235)%27%5D%2Clabel%3A%27Dataset%25201%27%2C%7D%2C%5D%2Clabels%3A%5B%27Online%27%2C%27In Person%27%5D%7D%7D";
+    let url = "https://quickchart.io/chart?bkg=white&c=%7Btype%3A%27pie%27%2Cdata%3A%7Bdatasets%3A%5B%7Bdata%3A%5B"+ in_person + "%2C" + online + "%5D%2CbackgroundColor%3A%5B%27rgb(232%2C56%2C79)%27%2C%27rgb(32%2C142%2C163)%27%5D%2Clabel%3A%27Dataset%25201%27%2C%7D%2C%5D%2Clabels%3A%5B%27Online%27%2C%27In Person%27%5D%7D%7D";
     console.log(url);
     return url;
 }
 
 function get_course_count_info() {
-    return "We define a course as a single section that a class meets during. In total, we found that there were " + parseInt(total_online,10).toLocaleString() + " online sections and " + parseInt(total_in_person,10).toLocaleString() + " in person sections, making " + parseInt(total_classes,10).toLocaleString() + " classes in all.";
+    return "A section is a single time that a class meets during. In total, we found that there were " + parseInt(total_online,10).toLocaleString() + " online sections and " + parseInt(total_in_person,10).toLocaleString() + " in person sections, making " + parseInt(total_classes,10).toLocaleString() + " sections in all.";
 }
 
 function get_seat_count_info() {
